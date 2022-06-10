c68c88a11b95ee2c9d4babcc2cfe0064059750f5
Adoyo <adoyoclifford@gmail.com>
2022-06-10 09:03:09 +0300

    second commit

--- Restaurant/app/src/main/java/com/example/restaurant/ui/theme/RestaurantScreen.kt
+++ Restaurant/app/src/main/java/com/example/restaurant/ui/theme/RestaurantScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -57,4 +58,10 @@ private fun RestaurantIcon(icon: ImageVector, modifier: Modifier) {
         contentDescription = "Restaurant icon",
         modifier = Modifier.padding(8.dp)
     )
+}
+
+@Preview
+@Composable
+fun RestaurantPreview() {
+    RestaurantScreen()
 }
